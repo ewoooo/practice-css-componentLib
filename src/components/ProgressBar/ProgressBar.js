00bf51f5544b0e1ff73df7a344d94c86f4e95390
ewoooo <leews7752@gmail.com>
2025-11-03 14:06:05 +0900

    setup Completed

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZE = {
+	small: { "--height": "4px" },
+	medium: { "--height": "6px" },
+	large: { "--height": "8px" },
+};
+
+const ProgressBase = styled.progress`
+	height: var(--height);
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+	const styles = SIZE[size];
+	const Element = ProgressBase;
+	return (
+		<>
+			<Element max={"100"} value={value} style={styles}></Element>
+		</>
+	);
 };
 
 export default ProgressBar;
